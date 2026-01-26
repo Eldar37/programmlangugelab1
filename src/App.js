7af61d4c907039389936e5e1c765ce1f1af7e102
Eldar37 <bekbosunov_e@iuca.kg>
2026-01-26 20:15:31 +0600

    Initial minimal React + Redux setup

--- src/App.js
+++ src/App.js
@@ -1,23 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
+import { useDispatch, useSelector } from 'react-redux';
+import { increment } from './features/counter/counterSlice';
 
 function App() {
+  const value = useSelector((state) => state.counter.value);
+  const dispatch = useDispatch();
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div>{value}</div>
+      <button onClick={() => dispatch(increment())}>Увеличить</button>
     </div>
   );
 }
