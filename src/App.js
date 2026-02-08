8af3d72c01868147e4b96f30439d3760f28ca965
Eldar37 <bekbosunov_e@iuca.kg>
2026-02-08 22:55:25 +0600

    Добавил header и навигацию.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
 import Header from './components/Header/Header';
+import { useDispatch, useSelector } from 'react-redux';
+import { increment } from './features/counter/counterSlice';
 import './App.css';
 
 function App() {
+  const value = useSelector((state) => state.counter.value);
+  const dispatch = useDispatch();
+
   return (
     <div className="app">
       <Header />
+      <main className="counter">
+        <div className="counter__value">{value}</div>
+        <button className="counter__button" onClick={() => dispatch(increment())}>
+          Увеличить
+        </button>
+      </main>
     </div>
   );
 }
