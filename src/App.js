4e932eebb5da3172dad9e16dd2e8f16a4aa9a4b6
Eldar37 <bekbosunov_e@iuca.kg>
2026-04-10 01:18:14 +0600

    Add Redux todo CRUD with detail view

--- src/App.js
+++ src/App.js
@@ -1,15 +1,13 @@
-import { useDispatch, useSelector } from 'react-redux';
+import { useSelector } from 'react-redux';
 import './App.css';
 import Header from './components/Header/Header';
 import AuthPanel from './components/AuthPanel/AuthPanel';
 import HomeContent from './components/HomeContent/HomeContent';
+import TodoBoard from './components/TodoBoard/TodoBoard';
 import { selectCurrentUser } from './features/auth/authSlice';
-import { increment } from './features/counter/counterSlice';
 
 function App() {
   const currentUser = useSelector(selectCurrentUser);
-  const value = useSelector((state) => state.counter.value);
-  const dispatch = useDispatch();
 
   return (
     <div className="app">
@@ -17,15 +15,7 @@ function App() {
       <main className="app__main">
         {currentUser ? (
           <>
-            <section className="counter">
-              <div>
-                <p className="counter__caption">Тестовый Redux-счётчик</p>
-                <div className="counter__value">{value}</div>
-              </div>
-              <button className="counter__button" onClick={() => dispatch(increment())}>
-                Увеличить
-              </button>
-            </section>
+            <TodoBoard />
             <HomeContent />
           </>
         ) : (
--- src/app/store.js
+++ src/app/store.js
@@ -3,13 +3,20 @@ import { authMiddleware } from './authMiddleware';
 import authReducer from '../features/auth/authSlice';
 import counterReducer from '../features/counter/counterSlice';
 import servicesReducer from '../features/services/servicesSlice';
+import todosReducer from '../features/todos/todoSlice';
+import { saveTodosState } from '../features/todos/todoStorage';
 
 export const store = configureStore({
   reducer: {
     auth: authReducer,
     counter: counterReducer,
     services: servicesReducer,
+    todos: todosReducer,
   },
   middleware: (getDefaultMiddleware) =>
     getDefaultMiddleware().concat(authMiddleware),
+});
+
+store.subscribe(() => {
+  saveTodosState(store.getState().todos.items);
 });
